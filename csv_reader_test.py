00306547ff69e051e7d5e3612bbb1ab46596258b
sh1n00 <rinjyou0031@toki.waseda.jp>
2022-11-22 15:16:28 +0900

    add: some files

---
+++ csv_reader_test.py
@@ -0,0 +1,21 @@
+import unittest
+from csv_reader import CSVPrinter
+
+class TestCSVPrinter(unittest.TestCase):
+    def test_read(self):
+        printer = CSVPrinter("sample.csv")
+        l = printer.read()
+        self.assertEqual(3, len(l))
+
+
+    def test_read_value(self):
+        correct = [
+            ['value1A', 'value1B', 'value1C'],
+            ['value2A', 'value2B', 'value2C'],
+            ['value3A', 'value3B', 'value3C']]
+        printer = CSVPrinter("sample.csv")
+        l = printer.read()
+        self.assertEqual(correct, l)
+    def test_file(self):
+        with self.assertRaises(FileNotFoundError):
+            CSVPrinter("sample1.csv").read()
