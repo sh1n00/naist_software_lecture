8d9d7f9ffd0831785996cc00a1cd06f180be9305
sh1n00 <rinjyou0031@toki.waseda.jp>
2022-11-24 16:04:16 +0900

    add: classを内部に移動

--- src/csv_reader_test.py
+++ src/csv_reader_test.py
@@ -1,14 +1,24 @@
 import unittest
 import sys
-sys.path.append("./")
-from csv_reader import CSVPrinter
+
+import csv
+
+class CSVPrinter:
+    def __init__(self, file_name):
+        self.file_name = file_name
+    
+    def read(self):
+        with open(self.file_name) as f:
+            reader = csv.reader(f)
+            lines = [row for row in reader]
+        return lines
+
 
 class TestCSVPrinter(unittest.TestCase):
     def test_read(self):
         printer = CSVPrinter("sample.csv")
         l = printer.read()
         self.assertEqual(3, len(l))
-
 
     def test_read_value(self):
         correct = [
